431514f407219a6a3900a6f3fde304b29bb148d4
DziNeIT <o.dzineit@gmail.com>
2016-06-02 14:27:06 +0100

    Basics done...
    Signed-off-by: DziNeIT <o.dzineit@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CraftQuesty
-Questy in the form of a Bukkit plugin to demonstrate how it can be used
+Questy in the form of a Bukkit plugin to demonstrate how it can be used.
+
+CraftQuesty does not use maven due to the legal issues involved with distributing CraftBukkit or forks of it. Apologies for any inconvenience as a result of this fact. Dependencies must be added locally.
---
+++ src/main/java/pw/ollie/questy/craftquesty/CraftQuestyPlugin.java
@@ -0,0 +1,16 @@
+package pw.ollie.questy.craftquesty;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main plugin class for CraftQuesty, a Bukkit plugin which uses the Questy Java framework to implement some simple quests as a manner of showing how Questy can be used.
+ */
+public final class CraftQuestyPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
